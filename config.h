73864223476e85c24434ca4566f3639de51ce295
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2020-01-06 20:46:03 -0800

    Add theme file and #include

--- config.h
+++ config.h
@@ -83,34 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"#000000", 	// black
-	"#d54e53",	// red
-	"#b9ca4a",	// green
-	"#e6c547",	// yellow
-	"#7aa6da",	// blue
-	"#c397d8",	// magenta
-	"#70c0ba",	// cyan
-	"#eaeaea",	// white
-
-	/* 8 bright colors */
-	"#666666", 	// black
-	"#ff3334",	// red
-	"#9ec400",	// green
-	"#e7c547",	// yellow
-	"#7aa6da",	// blue
-	"#b77ee0",	// magenta
-	"#54ced6",	// cyan
-	"#ffffff",	// white
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#eaeaea",	// foreground
-	"#000000",	// background
-};
-
+#include "theme.h"
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
---
+++ theme.h
@@ -0,0 +1,27 @@
+static const char *colorname[] = {
+    /* 8 normal colors */
+    "#242424",  // black
+    "#d54e53",  // red
+    "#b9ca4a",  // green
+    "#e6c547",  // yellow
+    "#7aa6da",  // blue
+    "#c397d8",  // magenta
+    "#70c0ba",  // cyan
+    "#eaeaea",  // white
+
+    /* 8 bright colors */
+    "#666666",  // black
+    "#ff3334",  // red
+    "#9ec400",  // green
+    "#e7c547",  // yellow
+    "#7aa6da",  // blue
+    "#b77ee0",  // magenta
+    "#54ced6",  // cyan
+    "#dedede",  // white
+
+    [255] = 0,
+
+    /* more colors can be added after 255 to use with DefaultXX */
+    "#eaeaea",  // foreground
+    "#000000",  // background
+};
